a6d02faf8bda8a24bf32807988a8b9b457f3c034
tkdhmin <mdh38112@sogang.ac.kr>
2025-03-23 17:14:13 +0900

    fix: reflect changed script name

--- milvus-expr/scripts/test.sh
+++ milvus-expr/scripts/test.sh
@@ -7,9 +7,9 @@ NUM=1000000
 # sudo sysctl -w vm.block_dump=1
 
 # Experiments
-python run_insert_expr.py --num "${NUM}"
-python run_index_build_expr.py --num "${NUM}"
-python run_index_load_expr.py --num "${NUM}"
-python run_search_expr.py --num "${NUM}"
+python run_insert_vectors_expr.py --num "${NUM}"
+python run_build_index_expr.py --num "${NUM}"
+python run_load_index_load_expr.py --num "${NUM}"
+python run_search_vectors_expr.py --num "${NUM}"
 
 # sudo sysctl -w vm.block_dump=0
